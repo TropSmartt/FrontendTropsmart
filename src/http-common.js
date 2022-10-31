9d6a15ac0617b73b70916907d6d3710c2f45b32a
JM1310 <lali151200@gmail.com>
2022-10-31 01:06:38 -0500

    feat: rutas

--- src/http-common.js
+++ src/http-common.js
@@ -3,9 +3,9 @@ import axios from 'axios';
 //n-word: json-server --watch db.json --routes routes.json
 
 export default axios.create({
-    baseURL: "https://ts-opensource-be.herokuapp.com/",
+    //baseURL: "https://ts-opensource-be.herokuapp.com/",
     //baseURL:"https://ts-appsweb-be.azurewebsites.net/",
-    //baseURL:"http://localhost:8080/",
+    baseURL:"http://localhost:8085/",
     params: {"Access-Control-Allow-Origin": "*"},
     headers: {
         "Content-type": "application/json",
