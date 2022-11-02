f1171f8924026b499ba223ce874cac87a03d60eb
Ale Abad <aleabad2016@gmail.com>
2022-11-01 23:25:36 -0500

    update backend url

--- src/http-common.js
+++ src/http-common.js
@@ -5,7 +5,11 @@ import axios from 'axios';
 export default axios.create({
     //baseURL: "https://ts-opensource-be.herokuapp.com/",
     //baseURL:"https://ts-appsweb-be.azurewebsites.net/",
-    baseURL:"http://localhost:8085/",
+    //baseURL:"http://localhost:8085/",
+
+    //HEROKU BACKEND
+    baseurl: "https://backend-tropsmart.herokuapp.com/",
+
     params: {"Access-Control-Allow-Origin": "*"},
     headers: {
         "Content-type": "application/json",
